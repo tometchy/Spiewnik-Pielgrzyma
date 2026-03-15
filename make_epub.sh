e16aa72071fe69e1a784f583e92b23c4133d5326
Tometchy <tometchy@gmail.com>
2026-03-15 22:24:12 +0100

    It works

--- make_epub.sh
+++ make_epub.sh
@@ -2,4 +2,8 @@ python3 make_epub.py
 
 ebook-convert book/index.html spiewnik-pielgrzyma.epub \
 --chapter "//h1" \
---level1-toc "//h1"
+--level1-toc "//h1" \
+--max-toc-links=2000 \
+--dont-split-on-page-breaks \
+--no-default-epub-cover \
+--toc-filter "Śpiewnik"
